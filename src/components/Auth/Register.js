44e8b6b226218904d95ce293e900faeb5a06c125
jonathan-crepeau <jon.crepeau@gmail.com>
2020-03-11 09:35:45 -0700

    updated Register.js, input tags didn't have '/>' for ending tag

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -34,20 +34,20 @@ class RegisterModal extends React.Component {
             </Form.Field>
             <Form.Field>
               <label>Password</label>
-              <input id="password" name="password" type="password" placeholder="Password" onChange={this.inputChange} value={this.state.password}></input>
+              <input id="password" name="password" type="password" placeholder="Password" onChange={this.inputChange} value={this.state.password} />
             </Form.Field>
             <Form.Field>
               <label>Email</label>
-              <input id="email" name="email" type="email" placeholder="Email" onChange={this.inputChange} value={this.state.email}></input>
+              <input id="email" name="email" type="email" placeholder="Email" onChange={this.inputChange} value={this.state.email} />
             </Form.Field>
             <Form.Field>
               <label>Public Email</label>
-              <input id="publicEmail" name="publicEmail" type="email" placeholder="Public Email" onChange={this.inputChange} value={this.state.publicEmail}></input>
+              <input id="publicEmail" name="publicEmail" type="email" placeholder="Public Email" onChange={this.inputChange} value={this.state.publicEmail} />
               <small>Your 'Public Email' is the email address shared with the other user at time of purchase. It can be the same email as you provided for the text field above ("Email")</small>
             </Form.Field>
             <Form.Field>
               <label>Zipcode</label>
-              <input id="zipcode" name="zipcode" type="text" placeholder="Zipcode" onChange={this.inputChange} value={this.state.zipcode}></input>
+              <input id="zipcode" name="zipcode" type="text" placeholder="Zipcode" onChange={this.inputChange} value={this.state.zipcode} />
             </Form.Field>
             <Button className="submit-button" type="submit" color="green" circular>Submit</Button><br/>
             <a className="login-link" href="#">Already have an account? Click here to login!</a>
