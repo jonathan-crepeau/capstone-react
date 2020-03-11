803a6263350916b161bf75d45fe686cba930320f
jonathan-crepeau <jon.crepeau@gmail.com>
2020-03-11 13:55:47 -0700

    created Favorites component

---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import axios from 'axios';
+
+import './Favorites.css';
+
+class Favorites extends React.Component {
+  state = {
+    postContent: []
+  }
+
+  // componentDidMount
+  // axios call to pull favorites for current user profile
+
+  //render()
+  // post list
+
+
+}
+
+
+export default Favorites;
